023bfa8c7d2afb7c79b827f289c3a623375f23ad
wangzixiao <UPC_wangzixiao@outlook.com>
2025-05-10 16:21:06 +0800

    update

--- Scripts/Plugins/RandomSprite2D.gd
+++ Scripts/Plugins/RandomSprite2D.gd
@@ -1,18 +1,19 @@
+@tool
+
 class_name RandomAnimatedSprite2D
 extends Sprite2D
-
-@export var is_environment : bool = false
 
 var max_frame : int = 0
 var frame_to_refresh : int = 18
 var refresh_frame : int = 0
 var pre_anim_frame : int = 0
 
+
 func _ready() -> void:
 	max_frame = hframes * vframes
 	pre_anim_frame = frame
-	pass
 
+	
 func _process(_delta: float) -> void:
 	# 更新动画帧
 	refresh_frame += 1
@@ -27,14 +28,4 @@ func _process(_delta: float) -> void:
 			
 		frame = next_frame
 		pre_anim_frame = frame
-		
-#	# 更新场景的order
-#	var main_charactor : MainCharactor = GameGlobal.GMainCharactor
-#	if is_environment and main_charactor:
-#		if global_position.y > main_charactor.global_position.y:
-#			z_index = 10
-#		else:
-#			z_index = main_charactor.z_index - 1
-#	else:
-#		z_index = 1
-		
+	
