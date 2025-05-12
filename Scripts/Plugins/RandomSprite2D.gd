e08448bd650d84304a499764e5a9ab853b548d60
wangzixiao <UPC_wangzixiao@outlook.com>
2025-05-12 15:19:26 +0800

    更新刷新规则

--- Scripts/Plugins/RandomSprite2D.gd
+++ Scripts/Plugins/RandomSprite2D.gd
@@ -3,22 +3,23 @@
 class_name RandomAnimatedSprite2D
 extends Sprite2D
 
-var max_frame : int = 0
-var frame_to_refresh : int = 18
-var refresh_frame : int = 0
-var pre_anim_frame : int = 0
+var max_frame: int = 0
+var time_to_refresh: float = 0.12
+var refresh_remain_time: float = 0
 
+var pre_anim_frame: int = 0
 
 func _ready() -> void:
 	max_frame = hframes * vframes
+	refresh_remain_time = time_to_refresh
 	pre_anim_frame = frame
 
 	
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	# 更新动画帧
-	refresh_frame += 1
-	if refresh_frame >= frame_to_refresh:
-		refresh_frame = 0
+	refresh_remain_time -= delta
+	if refresh_remain_time <= 0:
+		refresh_remain_time = time_to_refresh
 
 		var next_frame : int = randi() % hframes
 		if pre_anim_frame == next_frame:
